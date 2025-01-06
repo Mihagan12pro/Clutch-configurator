b2150dd35d6364e6e441d88bbfc61a39001af08d
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-01-06 15:37:22 +0300

    The sketches of the screw and the ring were updated.

--- resource.h
+++ resource.h
@@ -12,10 +12,11 @@
 #define IDD_DIALOG1                     316
 #define IDD_CHOOSE_ASSEMBLE             316
 #define ID_RING_BITMAP                  318
-#define IDB_RING_BITMAP                 318
-#define IDB_SCREW_BITMAP                319
 #define IDB_BITMAP2                     321
 #define IDB_COLLAR_BITMAP               321
+#define IDB_RING_BITMAP                 322
+#define IDB_BITMAP3                     323
+#define IDB_SCREW_BITMAP                323
 #define IDC_Sketch_PICTURE              1000
 #define IDC_save_STATIC                 1002
 #define IDC_EDIT5                       1006
@@ -80,7 +81,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        322
+#define _APS_NEXT_RESOURCE_VALUE        324
 #define _APS_NEXT_COMMAND_VALUE         32787
 #define _APS_NEXT_CONTROL_VALUE         1064
 #define _APS_NEXT_SYMED_VALUE           310
