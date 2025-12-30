82fb791c726fc831a43450b8ceb4c0c63c2939fa
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-12-30 19:07:52 +0300

    chore: add .gitignore

--- CClutchAssembler.h
+++ CClutchAssembler.h
@@ -1,8 +1,8 @@
 #pragma once
 #include"CSaveConfigurationsDlg.h"
-#include "C:\Program Files\ASCON\KOMPAS-3D v22 Study\SDK\Include\ksConstants.h"
-#include "C:\Program Files\ASCON\KOMPAS-3D v22 Study\SDK\Include\ksConstants3D.h"
-#import "C:\Program Files\ASCON\KOMPAS-3D v22 Study\SDK\lib\kAPI5.tlb"
+#include "C:\Program Files\ASCON\KOMPAS-3D v24 Study\SDK\Include\ksConstants.h"
+#include "C:\Program Files\ASCON\KOMPAS-3D v24 Study\SDK\Include\ksConstants3D.h"
+#import "C:\Program Files\ASCON\KOMPAS-3D v24 Study\SDK\lib\kAPI5.tlb"
 #include<string>
 #include "CClutchAssemble.h"
 #include "CChooseAssembleDlg.h"
